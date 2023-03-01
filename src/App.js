633cc210c84492a92b1c0b4ffd89df11138b271a
leequan <leequan2000@outlook.com>
2023-03-02 00:24:37 +0800

    Configuring the Router

--- src/App.js
+++ src/App.js
@@ -1,42 +1,9 @@
 import './App.css';
-import { useState, useEffect } from 'react';
 
-const GithubUser = ({ name, id, avatar }) => {
-  return (
-    <div>
-      <h1>{name}</h1>
-      <p>{id}</p>
-      <img src={avatar} alt={name} />
-    </div>
-  );
-};
-function App() {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    fetch(`https://api.github.com/users/lee-quan`)
-      .then((response) => response.json())
-      .then(setData)
-      .then(() => setLoading(false))
-      .catch(setError);
-  }, []);
-  if (loading) {
-    return <h1>Loading</h1>;
-  }
-  if (error) {
-    console.log(error);
-    return <h1>Error</h1>;
-  }
-  if (data) {
-    return (
-      <h1>
-        <GithubUser name={data.name} id={data.id} avatar={data.avatar_url} />
-      </h1>
-    );
-  }
+export function App() {
+  return <>APP</>;
 }
 
-export default App;
+export function About() {
+  return <>ABOUT</>;
+}
