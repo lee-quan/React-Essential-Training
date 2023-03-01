6ab038b26271edc83d0784c8f903b8ae39f171f4
leequan <leequan2000@outlook.com>
2023-03-01 13:57:17 +0800

    useRef

--- src/App.js
+++ src/App.js
@@ -1,42 +1,24 @@
 import './App.css';
-import { useState, useEffect, useReducer } from 'react';
+import { useRef } from 'react';
 
-// function App(props) {
-//   return (
-//     <div className="App">
-//       <h1>hello {props.library}</h1>
-//     </div>
-//   );
-// }
+function App() {
+  const txtTitle = useRef();
+  const hexColor = useRef();
 
-//Destructuring arrays and objects
-function App({ library }) {
-  const [emotion, setEmotion] = useState('sad');
-  const [checked, setChecked] = useReducer((checked) => !checked, false);
-  useEffect(() => {
-    console.log(emotion, 'without dependency array');
-  }, []);
-
-  useEffect(() => {
-    console.log(emotion, 'with dependency array');
-  }, [emotion]);
+  const submit = (e) => {
+    e.preventDefault();
+    const title = txtTitle.current.value;
+    const color = hexColor.current.value;
+    alert(`${title}, ${color}`);
+  };
 
   return (
-    <div className="App">
-      <h1>Hello {library}</h1>
-      <h1>I am {emotion}</h1>
-      <button onClick={() => setEmotion('happy')}>Happy</button>
-
-      <input type="checkbox" value={checked} onChange={setChecked} />
-      <label>{checked ? 'checked' : 'notChecked'}</label>
-    </div>
+    <form onSubmit={submit}>
+      <input type="text" placeholder="color title" ref={txtTitle} />
+      <input type="color" ref={hexColor} />
+      <button>Add</button>
+    </form>
   );
 }
-
-const cities = ['Tokyo', 'Shanghai'];
-console.log(cities[0]);
-
-const [tokyo] = ['Tokyo', 'Shanghai'];
-console.log(tokyo);
 
 export default App;
