1ae09aa2f9d00cbbf79fe7c99f4bb9165371b559
leequan <leequan2000@outlook.com>
2023-03-02 09:50:53 +0800

    React Testing

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,11 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+function timesTwo(a) {
+  return a * 2;
+}
+
+test('Multiplies by two', () => {
+  expect(timesTwo(4)).toBe(8);
+});
